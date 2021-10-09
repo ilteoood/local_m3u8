0e4f0dc185183915a1455e5134e4a6abb4cfc578
iLTeoooD <matteo.dazzi@mia-platform.eu>
2021-10-09 14:50:28 +0200

    Structure

--- internal/playlist/playlist_structure.go
+++ internal/playlist/playlist_structure.go
@@ -16,37 +16,37 @@ func stripPathToScan(fileCompletePath string) string {
 	return strings.Replace(fileCompletePath, pathToScan, "", 1)
 }
 
-func (playlist Playlist) AddPlaylistHeader() Playlist {
+func (playlist *Playlist) AddPlaylistHeader() *Playlist {
 	playlistName := fmt.Sprintf("#PLAYLIST:%s", env.RetrieveFileName())
 	return playlist.AddRow("#EXTM3U").AddRow(playlistName)
 }
 
-func (playlist Playlist) AddNewEntry(fileCompletePath string) Playlist {
+func (playlist *Playlist) AddNewEntry(fileCompletePath string) *Playlist {
 	return playlist.
 	AddInformation(fileCompletePath).
 	AddGroup(fileCompletePath).
 	AddFile(fileCompletePath)
 }
 
-func (playlist Playlist) AddInformation(fileCompletePath string) Playlist {
+func (playlist *Playlist) AddInformation(fileCompletePath string) *Playlist {
 	fileName := filepath.Base(fileCompletePath)
 	informationWithPrefix := fmt.Sprintf("#EXTINF:-1 tvg-name=\"%s\", %s", fileName, fileName)
 	return playlist.AddRow(informationWithPrefix)
 }
 
-func (playlist Playlist) AddGroup(fileCompletePath string) Playlist {
+func (playlist *Playlist) AddGroup(fileCompletePath string) *Playlist {
 	relativePath := filepath.Dir(stripPathToScan(fileCompletePath))
 	groupWithPrefix := fmt.Sprintf("#EXTGRP:%s", relativePath)
 	return playlist.AddRow(groupWithPrefix)
 }
 
-func (playlist Playlist) AddFile(fileCompletePath string) Playlist {
+func (playlist *Playlist) AddFile(fileCompletePath string) *Playlist {
 	baseUrl := env.RetrieveBaseUrl()
 	fileRelativePath := stripPathToScan(fileCompletePath)
 	return playlist.AddRow(baseUrl + fileRelativePath)
 }
 
-func (playlist Playlist) AddRow(rowContent string) Playlist {
+func (playlist *Playlist) AddRow(rowContent string) *Playlist {
 	playlist.Content = playlist.Content + rowContent + "\n"
 	return playlist
 }
